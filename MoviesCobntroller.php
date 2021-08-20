1a631438ac54efed5e651a3349f8b671fe7d4808
Daniel Klein <daniel.klein@videotron.ca>
2021-08-20 13:54:51 -0400

    Create MoviesCobntroller.php

---
+++ MoviesCobntroller.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\ViewModels\MovieViewModel;
+use App\ViewModels\MoviesViewModel;
+use Illuminate\Support\Facades\Http;
+
+
+class MoviesController extends Controller
+{
+    
+    public function index()
+    {
+
+
+        $popularMovies = Http::withToken('***token***')
+            ->get('https://api.themoviedb.org/3/movie/popular')
+            ->json()['results'];
+
+
+        $genres = Http::withToken('***token***')
+            ->get('https://api.themoviedb.org/3/genre/movie/list')
+            ->json()['genres'];
+
+
+        return view('movies.index', $viewModel);
+    }
+
+ 
+}
